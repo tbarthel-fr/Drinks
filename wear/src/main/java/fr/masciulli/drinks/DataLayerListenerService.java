e5c9c0d0c45e64957881a7f3868c4322f9ea2dd7
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2014-08-13 19:15:10 +0200

    Use service to launch activity on wearable

---
+++ wear/src/main/java/fr/masciulli/drinks/DataLayerListenerService.java
@@ -0,0 +1,38 @@
+package fr.masciulli.drinks;
+
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class DataLayerListenerService extends WearableListenerService {
+
+    private static final String TAG = "DataLayerListenerServic";
+
+    private static final String START_ACTIVITY_PATH = "/start-activity";
+    GoogleApiClient mGoogleApiClient;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        Log.d(TAG, "onMessageReceived: " + messageEvent);
+
+        // Check to see if the message is to start an activity
+        if (messageEvent.getPath().equals(START_ACTIVITY_PATH)) {
+            Intent startIntent = new Intent(this, WearDrinkActivity.class);
+            startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(startIntent);
+        }
+    }
+}
--- wear/src/main/java/fr/masciulli/drinks/WearDrinkActivity.java
+++ wear/src/main/java/fr/masciulli/drinks/WearDrinkActivity.java
@@ -12,7 +12,7 @@ import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Wearable;
 
-public class WearDrinkActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, MessageApi.MessageListener {
+public class WearDrinkActivity extends Activity {
 
     private TextView mTextView;
 
@@ -23,45 +23,5 @@ public class WearDrinkActivity extends Activity implements GoogleApiClient.Conne
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wear_drink);
         mTextView = (TextView)findViewById(R.id.text);
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Wearable.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Wearable.MessageApi.removeListener(mGoogleApiClient, this);
-        mGoogleApiClient.disconnect();
-    }
-
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        Log.d("WearDrinkActivity", "onConnected(): Successfully connected to Google API client");
-        Wearable.MessageApi.addListener(mGoogleApiClient, this);
-    }
-
-    @Override
-    public void onConnectionSuspended(int cause) {
-        Log.d("WearDrinkActivity", "onConnectionSuspended(): Connection to Google API client was suspended");
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        Log.e("WearDrinkActivity", "onConnectionFailed(): Failed to connect, with result: " + result);
-    }
-
-    @Override
-    public void onMessageReceived(MessageEvent event) {
-        Log.d("WearDrinkActivity", "onMessageReceived: " + event);
     }
 }
