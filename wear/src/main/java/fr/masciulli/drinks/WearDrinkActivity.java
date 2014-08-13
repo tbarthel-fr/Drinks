1e2fc2d2a2df51e1a19cdf74e5f7ac92b5bca09c
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2014-08-13 18:52:02 +0200

    Implement wearable-side message listening

--- wear/src/main/java/fr/masciulli/drinks/WearDrinkActivity.java
+++ wear/src/main/java/fr/masciulli/drinks/WearDrinkActivity.java
@@ -2,16 +2,66 @@ package fr.masciulli.drinks;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-public class WearDrinkActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+
+public class WearDrinkActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, MessageApi.MessageListener {
 
     private TextView mTextView;
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wear_drink);
         mTextView = (TextView)findViewById(R.id.text);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Wearable.MessageApi.removeListener(mGoogleApiClient, this);
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.d("WearDrinkActivity", "onConnected(): Successfully connected to Google API client");
+        Wearable.MessageApi.addListener(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.d("WearDrinkActivity", "onConnectionSuspended(): Connection to Google API client was suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.e("WearDrinkActivity", "onConnectionFailed(): Failed to connect, with result: " + result);
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent event) {
+        Log.d("WearDrinkActivity", "onMessageReceived: " + event);
     }
 }
