a3fef865ceb976f5745e4c653b6d9f316b24ffbe
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2014-08-14 17:33:01 +0200

    Display full drink

--- wear/src/main/java/fr/masciulli/drinks/WearDrinkActivity.java
+++ wear/src/main/java/fr/masciulli/drinks/WearDrinkActivity.java
@@ -3,26 +3,24 @@ package fr.masciulli.drinks;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.speech.tts.TextToSpeech;
+import android.text.Html;
 import android.widget.TextView;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.MessageApi;
-import com.google.android.gms.wearable.MessageEvent;
-import com.google.android.gms.wearable.Wearable;
 
 public class WearDrinkActivity extends Activity {
 
-    private TextView mTextView;
+    private TextView mNameView;
     private WearDrink mDrink;
+    private TextView mIngredientsView;
+    private TextView mInstructionsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wear_drink);
-        mTextView = (TextView)findViewById(R.id.text);
+        mNameView = (TextView)findViewById(R.id.title);
+        mIngredientsView = (TextView)findViewById(R.id.ingredients);
+        mInstructionsView = (TextView)findViewById(R.id.instructions);
         Intent intent = getIntent();
         if (intent.hasExtra("drink")) {
             mDrink = intent.getParcelableExtra("drink");
@@ -32,7 +30,19 @@ public class WearDrinkActivity extends Activity {
 
     private void updateViews() {
         if (mDrink != null) {
-            mTextView.setText(mDrink.name);
+            mNameView.setText(mDrink.name);
+            mInstructionsView.setText(mDrink.instructions);
+
+            String htmlString = "";
+            int i = 0;
+            for (String ingredient : mDrink.ingredients) {
+                if (++i == mDrink.ingredients.size()) {
+                    htmlString += "&#8226; " + ingredient;
+                } else {
+                    htmlString += "&#8226; " + ingredient + "<br>";
+                }
+            }
+            mIngredientsView.setText(Html.fromHtml(htmlString));
         }
     }
 }
